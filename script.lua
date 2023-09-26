da3ce79dc2d63e2676c71739fc77aa1b692b1773
Bemplia <145816318+Bemplia@users.noreply.github.com>
2023-09-26 19:52:59 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("Build a boat", "RJTheme4")
+local Window = Library.CreateLib("Build a boat", "RJTheme8")
 
 local Main = Window:NewTab("Main")
 local Movement = Window:NewTab("Movement")
@@ -25,25 +25,7 @@ Main:NewButton("Rejoin", "Rejoin", function()
 game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
 end)
 
-Main:NewButton("AntiAFK", "AntiIdle", function()
-	local GC = getconnections or get_signal_cons
-	if GC then
-		for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
-			if v["Disable"] then
-				v["Disable"](v)
-			elseif v["Disconnect"] then
-				v["Disconnect"](v)
-			end
-		end
-	else
-		Players.LocalPlayer.Idled:Connect(function()
-			local VirtualUser = game:GetService("VirtualUser")
-			VirtualUser:CaptureController()
-			VirtualUser:ClickButton2(Vector2.new())
-		end)
-	end
-	if not (args[1] and tostring(args[1]) == 'nonotify') then notify('Anti Idle','Anti idle is enabled') end
-end)
+
 
 Main:NewButton("Remove Water", "", function()
     for i,v in pairs(game:GetDescendants()) do
@@ -61,17 +43,38 @@ Main:NewButton("Remove Trees", "", function()
     end
 end)
 
-Main:NewButton("Remove Lock", "", function()
-    game:GetService("Workspace").WhiteZone.Lock:Destroy()
-    game:GetService("Workspace").CamoZone.Lock:Destroy()
-    game:GetService("Workspace").MagentaZone.Lock:Destroy()
-    game:GetService("Workspace").BlackZone.Lock:Destroy()
-    game:GetService("Workspace")["Really redZone"].Lock:Destroy()
-    game:GetService("Workspace")["Really blueZone"].Lock:Destroy()
-    game:GetService("Workspace")["New YellerZone"].Lock:Destroy()
+Main:NewDropdown("Remove Lock", "Remove", {"White Zone", "Black Zone", "Red Zone", "Green Zone", "Blue Zone", "Yellow Zone", "Purple Zone"}, function(currentOption)
+    while currentOption == "White Zone" do
+        game:GetService("Workspace").WhiteZone.Lock:Destroy()
+        wait(0.2)
+    end
+    while currentOption == "Black Zone" do
+        game:GetService("Workspace").BlackZone.Lock:Destroy()
+        wait(0.2)
+    end
+    while currentOption == "Red Zone" do
+        game:GetService("Workspace")["Really redZone"].Lock:Destroy()
+        wait(0.2)
+    end
+    while currentOption == "Green Zone" do
+        game:GetService("Workspace").CamoZone.Lock:Destroy()
+        wait(0.2)
+    end
+    while currentOption == "Blue Zone" do
+        game:GetService("Workspace")["Really blueZone"].Lock:Destroy()
+        wait(0.2)
+    end
+    while currentOption == "Yellow Zone" do
+        game:GetService("Workspace")["New YellerZone"].Lock:Destroy()
+        wait(0.2)
+    end
+    while currentOption == "Purple Zone" do
+        game:GetService("Workspace").MagentaZone.Lock:Destroy()
+        wait(0.2)
+    end
 end)
 
-Main:NewToggle("AutoFarm", "AFKFarm", function(state)
+Main:NewToggle("AutoFarm Chest", "AFKFarm", function(state)
     if state then
         enable = true
         while enable == true do
@@ -159,6 +162,93 @@ Main:NewToggle("AutoFarm", "AFKFarm", function(state)
     end
 end)
 
+Main:NewToggle("AutoFarm", "AFKFarm", function(state)
+    if state then
+        enable1 = true
+        while enable1 == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-54.0039444, 84.9583664, 1762.224, -0.999866426, -0.00202333252, 0.0162199121, -9.91661331e-09, 0.992309213, 0.123783745, -0.0163456239, 0.123767212, -0.992176652)
+            wait(0.25)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            if enable1 == false then
+                break
+            end
+            wait(2.5)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-54.1079063, 64.3214493, 2502.07568, -0.999743223, 0.00296215084, -0.0224656258, -1.1368023e-08, 0.991419077, 0.130721673, 0.0226600692, 0.130688101, -0.991164565)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            wait(2)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-50.2122116, 66.7413025, 3245.00317, -0.999949038, 0.00155586773, -0.00997654069, -7.08470882e-09, 0.988056719, 0.154090747, 0.010097133, 0.154082894, -0.988006353)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            wait(2)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-44.0629883, 58.9831238, 4030.01929, -0.999466598, -0.00545828696, 0.0321981087, 3.74487286e-09, 0.985933602, 0.167137519, -0.0326574817, 0.16704838, -0.98540771)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            wait(2)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-56.8305092, 55.0657997, 4760.271, -0.999936342, -0.00129597797, 0.0112095848, -7.5520239e-09, 0.99338311, 0.114847697, -0.0112842526, 0.114840388, -0.993319869)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            wait(2)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-55.5864677, 44.2472954, 5584.37354, -0.999971807, -0.00156129419, 0.00734687224, -6.9717947e-09, 0.978156745, 0.207868531, -0.00751093682, 0.207862675, -0.978129208)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            wait(2)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-53.1503983, 58.9461632, 6374.46289, -0.999992728, 0.000802412455, -0.00372232939, 6.4981549e-09, 0.977545381, 0.210725069, 0.00380783435, 0.210723534, -0.977538288)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            wait(2)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-59.6752052, 57.7362709, 7198.58545, -0.999920428, 0.00258698268, -0.0123456102, 9.69319291e-09, 0.978742778, 0.205091611, 0.0126137454, 0.205075294, -0.978664935)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            wait(2)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-52.720974, 54.7904854, 7912.30859, -0.99999696, -0.000487447542, 0.00242911559, 1.03510036e-08, 0.980453551, 0.196750626, -0.00247754063, 0.19675003, -0.980450571)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            wait(0.1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            wait(2)
+            if enable1 == false then
+                break
+            end
+            game.Players.LocalPlayer.Character.Humanoid.Health = 0
+            wait(10)
+        end
+    else
+        enable1 = false
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+    end
+end)
+
 Movement:NewSlider("WalkSpeed", "WalkSpeed", 500, 16, function(WalkSpeed)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = WalkSpeed
 end)
@@ -177,18 +267,18 @@ end)
 
 Movement:NewToggle("Noclip", "Noclip", function(state)
     if state then
-        enable1 = true
-        while enable1 == true do
+        enable2 = true
+        while enable2 == true do
             game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = false
             game.Players.LocalPlayer.Character.LowerTorso.CanCollide = false
             game.Players.LocalPlayer.Character.UpperTorso.CanCollide = false
-            if enable1 == false then
+            if enable2 == false then
                 break
             end
             wait(0.5)
         end
     else
-        enable1 = false
+        enable2 = false
         game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = true
         game.Players.LocalPlayer.Character.LowerTorso.CanCollide = true
         game.Players.LocalPlayer.Character.UpperTorso.CanCollide = true
